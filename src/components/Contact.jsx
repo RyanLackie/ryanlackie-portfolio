9a68a58555c263a0f59ad6b585beda13d3d76821
Ryan Lackie <ryanwlackie@gmail.com>
2019-04-19 15:05:15 -0400

    4/19 Update
    Changes to the contact section.
    Maybe rethink they way the auto scroll works.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,18 +21,32 @@ export default class Portfolio extends Component {
                     </div>
                 </div>
 
-                <div className="inputContainer">
-                
-                    <div className="inputCol">
-                        <input className='form-control input'></input>
+                <div className="message">
+                    Want to talk? <br/>
+                    I'm always up to get a cup of coffee.
+                </div>
 
-                        <input className='form-control input'></input>
 
-                        <input className='form-control input'></input>
+                <div className="container-fluid">
+
+                    <div className="row justify-content-center">
+                    
+                        <div className="col-lg-6 inputCol">
+                            <input className='form-control smInput' placeholder="Your Name"></input>
+
+                            <input className='form-control smInput' placeholder="Your Email"></input>
+
+                            <input className='form-control smInput' placeholder="Your Phone #"></input>
+                        </div>
+
+                        <div className="col-lg-6 inputCol">
+                            <textarea className='form-control lgInput' placeholder="Your Message"></textarea>
+                        </div>
+
                     </div>
 
-                    <div className="inputCol">
-                        <textarea className='form-control input'></textarea>
+                    <div className="buttonContainer">
+                        <button className='btn btn-warning sendBtn'>Send Message</button>
                     </div>
 
                 </div>
