abfb08cc78865a0cd9ae51c21b615ace10c71cd8
Ryan Lackie <RyanWLackie@gmail.com>
2021-04-14 09:02:14 -0400

    env heroku debugging

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use('/app', controller);
 
 // Server Stuff
 const server = 'localhost';
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
 
 // Port listen and message
 app.listen(port);
