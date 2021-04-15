ba58fc0015ae7f917b13fade070e5cc19f13d077
Ryan Lackie <RyanWLackie@gmail.com>
2021-04-15 14:53:14 -0400

    root

--- src/services/api_access.js
+++ src/services/api_access.js
@@ -1,4 +1,4 @@
-const protocol = window.location.hostname === 'localhost' ? 'http://' : 'https://'
+const protocol = window.location.hostname === 'localhost' ? 'http://' : 'http://'
 const apiHost = protocol+window.location.hostname;
 const port = apiHost.includes('localhost') ? ':8000' : ':80';
 const apiRoot = apiHost + port;
